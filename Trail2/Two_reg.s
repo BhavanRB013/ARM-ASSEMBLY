7f391996fead0c58257934aa8f54453555955567
BhavanRB013 <bhavanrb567@gmail.com>
2024-12-23 16:14:04 +0530

    Trail codes

---
+++ Trail2/Two_reg.s
@@ -0,0 +1,34 @@
+         
+		 AREA    RESET, DATA, READONLY
+          EXPORT  __Vectors
+ 
+__Vectors 
+	  DCD  0x20001000     ; stack pointer value when stack is empty
+			;The processor uses a full descending stack. 
+			;This means the stack pointer holds the address of the last 
+			;stacked item in memory. When the processor pushes a new item 
+			;onto the stack, it decrements the stack pointer and then 
+			;writes the item to the new memory location.
+
+          DCD  Reset_Handler  ; reset vector
+  
+          ALIGN
+ 
+; The program
+; Linker requires Reset_Handler
+ 
+          AREA    MYCODE, CODE, READONLY
+ 
+   	  ENTRY
+   	  EXPORT Reset_Handler
+ 
+ 
+Reset_Handler
+	
+	MOV R1,#25
+	MOV R10,#34
+HERE	
+	ADD R3,R1,R10
+	B HERE
+	END
+		
