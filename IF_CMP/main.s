ac7ebee2b5aacd7249c34a69b4b1dd1bc7adf414
BhavanRB013 <bhavanrb567@gmail.com>
2024-12-30 00:09:57 +0530

    About CMP and TST

---
+++ IF_CMP/main.s
@@ -0,0 +1,40 @@
+;;; Directives
+          ;PRESERVE8
+          ;THUMB       
+ 
+; Vector Table Mapped to Address 0 at Reset
+; Linker requires __Vectors to be exported
+ 
+          AREA    RESET, DATA, READONLY
+          EXPORT  __Vectors
+ 
+__Vectors 
+	  DCD  0x20001000     ; stack pointer value when stack is empty
+			;The processor uses a full descending stack. 
+			;This means the stack pointer holds the address of the last 
+			;stacked item in memory. When the processor pushes a new item 
+			;onto the stack, it decrements the stack pointer and then 
+			;writes the item to the new memory location.
+
+          DCD  Reset_Handler  ; reset vector
+  
+          ALIGN
+ 
+; The program
+; Linker requires Reset_Handler
+ 
+          AREA    PROGRAM22, CODE, READONLY
+ 
+   	  ENTRY
+   	  EXPORT Reset_Handler
+ 
+ 
+Reset_Handler
+
+	MOV R1,#20
+	MOV R2,#30
+	CMP R1,R2
+	BLO L1
+	MOV R3,#5
+L1	MOV R3,#2
+	END
